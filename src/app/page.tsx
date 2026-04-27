f2d35112ef91e5d67a4ff4e8c3983364a85a0c5a
go-tiger <dev.gotiger@gmail.com>
2026-04-27 10:40:36 +0900

    feat: ABOUT 섹션 구현
    - 타임라인 좌우 번갈아 배치
    - 중앙 세로선 + 점 (브라운 그라데이션)
    - 스크롤 진입 애니메이션 (Intersection Observer)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
 import HomeSection from '@/components/Home'
+import About from '@/components/About'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main>
         <HomeSection />
+        <About />
         {/* 섹션 추가 예정 */}
       </main>
     </>
